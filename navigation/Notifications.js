807b78bd88dddd92ad85044369472318ade9c845
tusharchauhan94 <tushar.chauhan94@utexas.edu>
2021-01-02 13:52:46 -0600

    Notifications received but not working

--- navigation/Notifications.js
+++ navigation/Notifications.js
@@ -4,14 +4,18 @@ import messaging from '@react-native-firebase/messaging';
 import AppStack from './AppStack';
 
 const Notifications = () => {
+  
     useEffect(() => {
         const unsubscribe = messaging().onMessage(async remoteMessage => {
-       console.log("new message" , remoteMessage);
+        console.log("new message" , remoteMessage);
+        return (<AppStack />);
         // show the message to the user or use it to update to your local store
        return unsubscribe;
       }, []);
+      
       messaging().setBackgroundMessageHandler(async remoteMessage => {
         console.log('Message handled in the background!', remoteMessage);
+        return (<AppStack />);
       });
     });
     
